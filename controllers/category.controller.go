0a4dda4ae76c07f5e21e6744e21d00b0184c5c89
iamtonmoy0 <tonmoy2code@gmail.com>
2024-04-06 23:00:48 +0600

    "Add controllers for getting, creating, updating, and deleting categories with GORM"
    This commit adds functionality to the category controller in the "go-sales-api" project. It includes the following changes:
    
    1. `CreateCategoryController`: Creates a new category record in the database.
    2. `GetAllCategoryController`: Retrieves all category records from the database and returns them in the response.
    3. `GetSingleCategoryController`: Retrieves a single category record from the database using its ID and returns it in the response. If the category is not found, it returns a 404 Not Found response.
    4. `UpdateCategoryController`: Updates an existing category record in the database with new data from the request body. If the category is not found, it returns a 409 Conflict response.
    5. `DeleteCategoryController`: Deletes a category record from the database using its ID and returns a success message in the response. If the category is not found, it returns a 500 Internal Server Error response.
    
    These changes utilize the GORM ORM to interact with the database.

--- controllers/category.controller.go
+++ controllers/category.controller.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	database "github.com/iamtonmoy0/go-sales-api/config"
 	"github.com/iamtonmoy0/go-sales-api/models"
+	"gorm.io/gorm"
 )
 
 // create category
@@ -27,13 +28,73 @@ func CreateCategoryController(c *fiber.Ctx) error {
 }
 
 // get all category
-func GetAllCategoryController(c *fiber.Ctx) error { return nil }
+func GetAllCategoryController(c *fiber.Ctx) error {
+	db := database.Database()
+	records := []*models.Category{}
+	// query
+	query := db.Find(&records)
+	if query.Error != nil {
+		log.Fatal("Failed to fetch categories: ", query.Error)
+		c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to fetch categories",
+		})
+	}
+	c.Status(http.StatusOK).JSON(fiber.Map{
+		"data": records,
+	})
+	return nil
+}
 
 // get single category
-func GetSingleCategoryController(c *fiber.Ctx) error { return nil }
+func GetSingleCategoryController(c *fiber.Ctx) error {
+	db := database.Database()
+	id := c.Params("id")
+	var record models.Category
+	result := db.First(&record, id)
+	if result.Error == gorm.ErrRecordNotFound {
+		c.Status(http.StatusNotFound).JSON(fiber.Map{
+			"error": "category not found",
+		})
+	}
+	c.Status(http.StatusOK).JSON(fiber.Map{"msg": ""})
+	return nil
+}
 
 // update category
-func UpdateCategoryController(c *fiber.Ctx) error { return nil }
+func UpdateCategoryController(c *fiber.Ctx) error {
+	db := database.Database()
+	var record models.Category
+	id := c.Params("id")
+	err := c.BodyParser(&record)
+	if err != nil {
+		c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid payload",
+		})
+	}
+	//  check if the user exists in the database
+	res := db.Model(&models.Category{}).Where("id = ?", id).Updates(&record)
+	if res.Error != nil {
+		c.Status(http.StatusConflict).JSON(fiber.Map{
+			"error": "failed to update category",
+		})
+	}
+	c.Status(http.StatusOK).JSON(fiber.Map{
+		"msg": "successfully updated the category",
+	})
+	return nil
+}
 
 // delete category
-func DeleteCategoryController(c *fiber.Ctx) error { return nil }
+func DeleteCategoryController(c *fiber.Ctx) error {
+	db := database.Database()
+	id := c.Params("id")
+	record := &models.Category{}
+	result := db.Delete(record, id)
+	if result.Error != nil {
+		c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to delete category",
+		})
+	}
+	c.Status(http.StatusOK).JSON(fiber.Map{"msg": "Successfully deleted Category!"})
+	return nil
+}
